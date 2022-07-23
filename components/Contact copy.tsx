613a624d0c1026b17e3229d50aad55cc272fb926
RechadSalma <rechadsalma123@gmail.com>
2022-07-23 18:01:35 +0100

    f34

---
+++ components/Contact copy.tsx
@@ -0,0 +1,146 @@
+import React, { useEffect } from "react";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
+
+import styles from "../styles/components/Contact.module.scss";
+
+type Props = {};
+
+export default function Contact({}: Props) {
+  const { Contact_component } = styles;
+
+  const encode = (data: any) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+
+  useEffect(() => console.log(schema));
+
+  // handle validation from Yum
+  const schema = yup.object().shape({
+    name: yup.string().required("Required"),
+    email: yup.string().email(),
+    message: yup.string().required("Required"),
+  });
+
+  return (
+    <Formik
+      validationSchema={schema}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({
+            "form-name": "contact",
+            ...values,
+          }),
+        })
+          .then(() => {
+            alert("Success!");
+            setSubmitting(false);
+          })
+          .catch((error) => {
+            alert("Error: Please Try Again!");
+            setSubmitting(false);
+          });
+      }}
+      initialValues={{
+        name: "",
+        email: "",
+        message: "",
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+        // return form jsx component
+        <Form
+          noValidate
+          onSubmit={handleSubmit}
+          name="iKcontactForm"
+          method="POST"
+          action="/"
+          // data-netlify="true"
+          // netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="iKcontactForm" />
+
+          {/* name text box */}
+          <Form.Group>
+            <Form.Label>
+              Name <span>*</span>
+            </Form.Label>
+            {/* is a controlled form */}
+            <Form.Control
+              // as="textarea"
+              type="text"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+              isInvalid={!!errors.name}
+              placeholder="Enter your name"
+            />
+            {/* will display to user their input in the textarea is invalid */}
+            <Form.Control.Feedback type="invalid">
+              {errors.name}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          {/* email text box */}
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+
+            {/* is a controlled form */}
+            <Form.Control
+              // as="textarea"
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              isInvalid={!!errors.email}
+              placeholder="Enter your email"
+            />
+            {/* will display to user their input in the textarea is invalid */}
+            <Form.Control.Feedback type="invalid">
+              {errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          {/* message textarea box */}
+          <Form.Group>
+            <Form.Label>
+              Message <span>*</span>
+            </Form.Label>
+            {/* is a controlled form */}
+            <Form.Control
+              as="textarea"
+              name="message"
+              value={values.message}
+              onChange={handleChange}
+              isInvalid={!!errors.message}
+              placeholder="Enter your message"
+            />
+            {/* will display to user their input in the textarea is invalid */}
+            <Form.Control.Feedback type="invalid">
+              {errors.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Button type="submit">submit message</Button>
+          {/* <Button onClick={() => setShowCreateCommentBox(false)}>Cancel</Button> */}
+        </Form>
+      )}
+    </Formik>
+  );
+}
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
@@ -8,139 +8,68 @@ import styles from "../styles/components/Contact.module.scss";
 type Props = {};
 
 export default function Contact({}: Props) {
-  const { Contact_component } = styles;
+  const [validated, setValidated] = useState(false);
 
-  const encode = (data: any) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  };
+  // const handleSubmit = (event: any) => {
+  //   const form = event.currentTarget as HTMLFormElement;
+  //   if (form.checkValidity() === false) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //   }
 
-  useEffect(() => console.log(schema));
-
-  // handle validation from Yum
-  const schema = yup.object().shape({
-    name: yup.string().required("Required"),
-    email: yup.string().email(),
-    message: yup.string().required("Required"),
-  });
+  //   setValidated(true);
+  // };
 
   return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
-        // fetch("/success", {
-        //   method: "POST",
-        //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        //   body: encode({
-        //     "form-name": "contact",
-        //     ...values,
-        //   }),
-        // })
-        //   .then(() => {
-        //     alert("Success!");
-        //     setSubmitting(false);
-        //   })
-        //   .catch((error) => {
-        //     alert("Error: Please Try Again!");
-        //     setSubmitting(false);
-        //   });
-      }}
-      initialValues={{
-        name: "",
-        email: "",
-        message: "",
-      }}
+    <Form
+      noValidate
+      // validated={validated}
+      // onSubmit={handleSubmit}
+      name="iKcontactForm"
+      method="POST"
     >
-      {({
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        touched,
-        isValid,
-        errors,
-      }) => (
-        // return form jsx component
-        <Form
-          noValidate
-          onSubmit={handleSubmit}
-          name="iKcontactForm"
-          method="POST"
-          action="/"
-          // data-netlify="true"
-          // netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="iKcontactForm" />
+      <input type="hidden" name="form-name" value="iKcontactForm" />
 
-          {/* name text box */}
-          <Form.Group>
-            <Form.Label>
-              Name <span>*</span>
-            </Form.Label>
-            {/* is a controlled form */}
-            <Form.Control
-              // as="textarea"
-              type="text"
-              name="name"
-              value={values.name}
-              onChange={handleChange}
-              isInvalid={!!errors.name}
-              placeholder="Enter your name"
-            />
-            {/* will display to user their input in the textarea is invalid */}
-            <Form.Control.Feedback type="invalid">
-              {errors.name}
-            </Form.Control.Feedback>
-          </Form.Group>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            name="name"
+            required
+            type="text"
+            placeholder="First name"
+            defaultValue="Mark"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
 
-          {/* email text box */}
-          <Form.Group>
-            <Form.Label>Email</Form.Label>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>email</Form.Label>
+          <Form.Control
+            required
+            name="email"
+            type="email"
+            placeholder="Last name"
+            defaultValue="ot@mail.com"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
 
-            {/* is a controlled form */}
-            <Form.Control
-              // as="textarea"
-              type="email"
-              name="email"
-              value={values.email}
-              onChange={handleChange}
-              isInvalid={!!errors.email}
-              placeholder="Enter your email"
-            />
-            {/* will display to user their input in the textarea is invalid */}
-            <Form.Control.Feedback type="invalid">
-              {errors.email}
-            </Form.Control.Feedback>
-          </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>message</Form.Label>
+          <Form.Control
+            required
+            name="message"
+            as="textarea"
+            // type="email"
+            placeholder="Last name"
+            defaultValue="Otto"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
 
-          {/* message textarea box */}
-          <Form.Group>
-            <Form.Label>
-              Message <span>*</span>
-            </Form.Label>
-            {/* is a controlled form */}
-            <Form.Control
-              as="textarea"
-              name="message"
-              value={values.message}
-              onChange={handleChange}
-              isInvalid={!!errors.message}
-              placeholder="Enter your message"
-            />
-            {/* will display to user their input in the textarea is invalid */}
-            <Form.Control.Feedback type="invalid">
-              {errors.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <Button type="submit">submit message</Button>
-          {/* <Button onClick={() => setShowCreateCommentBox(false)}>Cancel</Button> */}
-        </Form>
-      )}
-    </Formik>
+        <Button type="submit">Submit form</Button>
+      </Row>
+    </Form>
   );
 }
