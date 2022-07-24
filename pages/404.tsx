faa4b163aa55889277d07947e90f89621a266089
RechadSalma <rechadsalma123@gmail.com>
2022-07-24 13:58:56 +0100

    testing 404 with form submission

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import BackgroundParticles from "../components/BackgroundParticles";
 
 type Props = {};
 
 export default function Error404({}: Props) {
-  return <div>Error404</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  });
+
+  return (
+    <>
+      <BackgroundParticles />
+    </>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import BackgroundParticles from "../components/BackgroundParticles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* <BackgroundParticles /> */}
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
