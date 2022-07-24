b2393be8543146622c992227830c319216d4a074
RechadSalma <rechadsalma123@gmail.com>
2022-07-24 10:45:18 +0100

    234

--- next.config.js
+++ next.config.js
@@ -2,11 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return {
-      afterFiles: [{ source: "/:path*", destination: "/_404/:path*" }],
-    };
-  },
+  // async rewrites() {
+  //   return {
+  //     afterFiles: [{ source: "/:path*", destination: "/_404/:path*" }],
+  //   };
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       destination: "/",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
