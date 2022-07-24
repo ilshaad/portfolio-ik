03f9ebef5153bfe7af6ac34ea094576906e00744
RechadSalma <rechadsalma123@gmail.com>
2022-07-24 09:34:29 +0100

    44

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -10,67 +10,80 @@ type Props = {};
 export default function Contact({}: Props) {
   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (event: any) => {
-    const form = event.currentTarget as HTMLFormElement;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+  // const handleSubmit = (event: any) => {
+  //   const form = event.currentTarget as HTMLFormElement;
+  //   if (form.checkValidity() === false) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //   }
 
-    setValidated(true);
-  };
+  //   setValidated(true);
+  // };
+
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    if (window.location.search.includes("success=true")) {
+      setSuccess(true);
+    }
+  }, []);
 
   return (
-    <Form
-      noValidate
-      validated={validated}
-      onSubmit={handleSubmit}
-      name="iKportfolioContactForm"
-      method="POST"
-      action="/"
-    >
-      <input type="hidden" name="form-name" value="iKportfolioContactForm" />
+    <>
+      <Form
+        noValidate
+        // validated={validated}
+        // onSubmit={handleSubmit}
+        name="iKportfolioContactForm"
+        method="POST"
+        action="/?success=true"
+      >
+        <input type="hidden" name="form-name" value="iKportfolioContactForm" />
 
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            name="name"
-            required
-            type="text"
-            placeholder="First name"
-            defaultValue="Mark"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>First name</Form.Label>
+            <Form.Control
+              name="name"
+              required
+              type="text"
+              placeholder="First name"
+              defaultValue="Mark"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>email</Form.Label>
-          <Form.Control
-            required
-            name="email"
-            type="email"
-            placeholder="Last name"
-            defaultValue="ot@mail.com"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+            <Form.Label>email</Form.Label>
+            <Form.Control
+              required
+              name="email"
+              type="email"
+              placeholder="Last name"
+              defaultValue="ot@mail.com"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>message</Form.Label>
-          <Form.Control
-            required
-            name="message"
-            as="textarea"
-            // type="email"
-            placeholder="Last name"
-            defaultValue="Otto"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+            <Form.Label>message</Form.Label>
+            <Form.Control
+              required
+              name="message"
+              as="textarea"
+              // type="email"
+              placeholder="Last name"
+              defaultValue="Otto"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
 
-        <Button type="submit">Submit form</Button>
-      </Row>
-    </Form>
+          <Button type="submit">Submit form</Button>
+        </Row>
+      </Form>
+      {success && (
+        <p style={{ color: "green" }}>Successfully submitted form!</p>
+      )}
+    </>
   );
 }
