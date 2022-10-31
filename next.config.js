f8cd468454dd0d2333d47b34b94a23775cbda7aa
RechadSalma <rechadsalma123@gmail.com>
2022-10-31 15:01:23 +0000

    TD insert the instagram svg within the contact page =W=updated the symtax for next-pwa module & now pwa is working fine now

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // async rewrites() {
-  //   return {
-  //     afterFiles: [{ source: "/:path*", destination: "/_404/:path*" }],
-  //   };
-  // },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       destination: "/",
-  //     },
-  //   ];
-  // },
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   // async rewrites() {
+//   //   return {
+//   //     afterFiles: [{ source: "/:path*", destination: "/_404/:path*" }],
+//   //   };
+//   // },
+//   // async rewrites() {
+//   //   return [
+//   //     {
+//   //       source: "/:path*",
+//   //       destination: "/",
+//   //     },
+//   //   ];
+//   // },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
 
 // const withPWA = require("next-pwa");
 
@@ -29,3 +29,14 @@ module.exports = nextConfig;
 //     swcMinify: true,
 //   },
 // });
+
+// * this is the upgrade symtax for 'next-pwa@5.6.0'
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  // next.js config
+  reactStrictMode: true,
+  swcMinify: true,
+});
