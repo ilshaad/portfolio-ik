8b8a0757a0afb6e1f8637d20e228d2bce7041e76
ilshaad <82680145+ilshaad@users.noreply.github.com>
2025-06-01 11:44:40 +0100

    updated my blog site url

--- config/urls.ts
+++ config/urls.ts
@@ -5,7 +5,7 @@ const urls = {
   instagram: "https://www.instagram.com/ilshaad.k/",
   bugTrackerSite: "https://bug-tracker-frontend-ik-202203.herokuapp.com/",
   bugTrackerGitRepo: "https://github.com/ilshaad/bug-tracker-ik",
-  blogSite: "https://www.ilshaadblog.tk/",
+  blogSite: "https://my-blog-site-ik.vercel.app/",
   blogGitRepo: "https://github.com/ilshaad/my-blog-site-ik",
   portfolioGitRepo: "https://github.com/ilshaad/portfolio-ik",
 };
