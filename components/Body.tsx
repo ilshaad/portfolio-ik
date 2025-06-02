38e37a3d759dd4c223a48b818a1595f3e96af5d4
ilshaad <82680145+ilshaad@users.noreply.github.com>
2025-06-02 11:28:05 +0100

    use client for more pages

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
